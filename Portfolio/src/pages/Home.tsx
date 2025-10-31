15aa43299d05b349d48e447aa9f4cea369566e74
Wesley Lawson <lawsonwe@bc.edu>
2025-10-31 12:37:50 -0400

    fixing analytics deployment error

--- Portfolio/src/pages/Home.tsx
+++ Portfolio/src/pages/Home.tsx
@@ -7,7 +7,7 @@ import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const Home = () => {
 
