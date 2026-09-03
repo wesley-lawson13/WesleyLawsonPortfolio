d82e8246cf01301f36843b831111f2f0964bd71c
Wesley Lawson <wesleylawson13@gmail.com>
2026-09-02 20:38:58 -0400

    Updating Lembas Links in projects

--- Portfolio/src/components/ContactSection.jsx
+++ Portfolio/src/components/ContactSection.jsx
@@ -45,7 +45,7 @@ export const ContactSection = () => {
             for more updates coming soon.
           </i>
           <p className="text-dark">
-            <b>Last Update:</b> August 12, 2026
+            <b>Last Update:</b> September 2, 2026
           </p>
         </div>
       </div>
